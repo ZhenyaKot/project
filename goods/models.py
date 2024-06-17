f41dab19f916dcf0938b4d3bba4a76adccd8ef67
Евгений <>
2024-06-17 19:25:47 +0300

    after adding fixtures for goods

--- goods/models.py
+++ goods/models.py
@@ -10,6 +10,9 @@ class Categories(models.Model):
         verbose_name = 'Категорию'
         verbose_name_plural = 'Категории'
 
+    def __str__(self):
+        return self.name
+
 
 class Products(models.Model):
     name = models.CharField(max_length=150, unique=True, verbose_name='Название')
@@ -17,7 +20,7 @@ class Products(models.Model):
     description = models.TextField(blank=True, null=True, verbose_name='Описание')
     image = models.ImageField(upload_to='goods_images', null=True, blank=True, verbose_name='Изображение')
     price = models.DecimalField(max_digits=7, decimal_places=2, default=0.00, verbose_name='Цена')
-    discount = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='скидка в %')
+    discount = models.DecimalField(default=0.00, max_digits=4, decimal_places=2, verbose_name='скидка в %')
     quantity = models.PositiveIntegerField(default=0, verbose_name='Количество')
     compound = models.TextField(blank=True, null=True, verbose_name='Состав')
     additional_information = models.TextField(blank=True, null=True, verbose_name='Дополнительная информация')
@@ -29,6 +32,8 @@ class Products(models.Model):
         verbose_name = 'Продукт'
         verbose_name_plural = 'Продукты'
 
+    def __str__(self):
+        return f'{self.name} Количество - {self.quantity}'
 
 # class Products_Perfume(models.Model):
 #     name = models.CharField(max_length=150, unique=True, verbose_name='Название')
