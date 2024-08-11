58978317029495713d76c20130c466c78376f995
Евгений <>
2024-08-11 22:52:36 +0300

    The product filter on the site page and urlencode

--- goods/templatetags/goods_tags.py
+++ goods/templatetags/goods_tags.py
@@ -1,8 +1,18 @@
 from django import template
+from urllib.parse import urlencode
+
 from goods.models import Categories
 
 register = template.Library()
 
+
 @register.simple_tag
 def tag_categories():
     return Categories.objects.all()
+
+
+@register.simple_tag
+def change_params(request, **kwargs):
+    query = request.GET.copy()
+    query.update(kwargs)
+    return query.urlencode()
--- goods/views.py
+++ goods/views.py
@@ -9,14 +9,25 @@ from goods.models import Products
 
 def catalog(request, category_slug):
     page = request.GET.get('page', 1)
+    on_sale = request.GET.get('on_sale')
+    order_by = request.GET.get('order_by')
 
     if category_slug == 'all':
         goods = Products.objects.all()
     else:
-        goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
+        goods = Products.objects.filter(category__slug=category_slug)
+
+    if on_sale:
+        goods = goods.filter(discount__gt=0)
+
+    if order_by and order_by != 'default':
+        if order_by == 'price':
+            goods = goods.order_by(order_by)
+        elif order_by == '-price':
+            goods = goods.order_by(order_by)
 
     paginator = Paginator(goods, 3)
-    current_page = paginator.page(int(page))
+    current_page = paginator.get_page(page)
 
     context = {
         'title': 'Home - Каталог',
