c3df7f6e5b714097847b72e87723a19caa9e3ff5
Евгений <>
2024-08-12 22:25:41 +0300

    adding a price slider and writing its logic

--- goods/views.py
+++ goods/views.py
@@ -11,6 +11,8 @@ def catalog(request, category_slug):
     page = request.GET.get('page', 1)
     on_sale = request.GET.get('on_sale')
     order_by = request.GET.get('order_by')
+    lower_price_range = request.GET.get('lower_price_range')
+    upper_price_range = request.GET.get('upper_price_range')
 
     if category_slug == 'all':
         goods = Products.objects.all()
@@ -26,13 +28,29 @@ def catalog(request, category_slug):
         elif order_by == '-price':
             goods = goods.order_by(order_by)
 
+    if lower_price_range and upper_price_range:
+        goods = goods.filter(price__range=(lower_price_range, upper_price_range))
+
+        if not goods.exists():
+            paginator = Paginator(goods, 3)
+            current_page = paginator.get_page(page)
+            context = {
+                'title': 'Home - Каталог',
+                'goods': current_page,
+                'slug_url': category_slug,
+                'no_results': True
+            }
+
+            return render(request, 'goods/catalog.html', context)
+
     paginator = Paginator(goods, 3)
     current_page = paginator.get_page(page)
 
     context = {
         'title': 'Home - Каталог',
         'goods': current_page,
-        'slug_url': category_slug
+        'slug_url': category_slug,
+        'no_results': False
     }
 
     return render(request, 'goods/catalog.html', context)
